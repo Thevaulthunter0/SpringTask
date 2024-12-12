ec332e80f0d465cd4f200797a02f0788d0a69811
Mathieu <mathieuppicard@hotmail.com>
2024-12-12 00:50:38 -0500

    Ajout methodes get Task

--- src/main/java/com/project/springtask/Controller/TaskController.java
+++ src/main/java/com/project/springtask/Controller/TaskController.java
@@ -1,8 +1,16 @@
 package com.project.springtask.Controller;
 
+import com.project.springtask.Entity.Task;
+import com.project.springtask.Entity.Users;
 import com.project.springtask.Service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/task")
@@ -14,4 +22,36 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @GetMapping("getAll")
+    public ResponseEntity<Optional<List<Task>>> getAllTask()
+    {
+        Optional<List<Task>> oTasks = this.taskService.getAllTask();
+        if(oTasks.isPresent())
+        {
+            return ResponseEntity.ok(oTasks);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<Optional<Task>> getTaskById(@PathVariable Long id)
+    {
+        Optional<Task> oTask = this.taskService.getTaskById(id);
+        if(oTask.isPresent())
+        {
+            return ResponseEntity.ok(oTask);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("get/{id}user")
+    public ResponseEntity<Optional<Users>> GetUserFromTask(@PathVariable Long id)
+    {
+        Optional<Users> oUser = this.taskService.getUserFromTask(id);
+        if(oUser.isPresent())
+        {
+            return ResponseEntity.ok(oUser);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/project/springtask/Service/TaskService.java
+++ src/main/java/com/project/springtask/Service/TaskService.java
@@ -1,7 +1,12 @@
 package com.project.springtask.Service;
 
+import com.project.springtask.Entity.Task;
+import com.project.springtask.Entity.Users;
 import com.project.springtask.Repository.TaskRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -12,6 +17,19 @@ public class TaskService {
         this.taskRepo = taskRepo;
     }
 
-    //Implements all the service needed//
+    public Optional<List<Task>> getAllTask()
+    {
+        return Optional.of(taskRepo.findAll());
+    }
 
+    public Optional<Task> getTaskById(Long id)
+    {
+        return taskRepo.findById(id);
+    }
+
+    public Optional<Users> getUserFromTask(Long id)
+    {
+        Optional<Task> task = Optional.of(taskRepo.findById(id).get());
+        return Optional.of(task.get().getUsers());
+    }
 }
